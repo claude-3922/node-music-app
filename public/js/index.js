163efaf135042e18a8d0f44382a04358561d40ce
claude-3922 <aminabdulrehman532@gmail.com>
2024-07-28 11:04:08 +0500

    we have cool buttons now!(although they don't do anything yet)

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,60 @@
-function changeThumbnail(songId) {
+const audioPlayer = document.querySelector(".audioPlayer audio");
+const audioControls = document.querySelector(
+  ".audioPlayer .audioPlayer-controls"
+);
+
+function getSongData(songId) {
   const defaultUrl = "http://localhost:6060/images/no_thumbnail.png";
   fetch(`http://localhost:6060/songData/thumbnail?id=${songId}`)
     .then((res) => {
       res
         .text()
         .then((url) => {
-          document.getElementById("audioPlayer_thumbnail").setAttribute("src", url);
+          document
+            .querySelector(".audioPlayer-thumbnail")
+            .setAttribute("href", url);
+          document
+            .querySelector(
+              ".audioPlayer-thumbnail .audioPlayer-thumbnail-image"
+            )
+            .setAttribute("src", url);
         })
         .catch((err) => {
           console.log(err);
-          return defaultUrl;
+          setDefaultThumbnail();
         });
     })
     .catch((err) => {
       console.log(err);
-      return defaultUrl;
+      setDefaultThumbnail();
     });
+
+  function setDefaultThumbnail() {
+    document
+      .querySelector(".audioPlayer-thumbnail")
+      .setAttribute("href", defaultUrl);
+    document
+      .querySelector(".audioPlayer-thumbnail .audioPlayer-thumbnail-image")
+      .setAttribute("src", defaultUrl);
+  }
 }
+
+audioPlayer.addEventListener("play", () => {
+  const playButton = document.querySelector(
+    ".audioPlayer .audioPlayer-controls .audioPlayer-controls-playbackButton"
+  );
+
+  playButton.setAttribute("title", "Pause")
+  playButton.setAttribute("src", "http://localhost:6060/icons/pause.svg");
+});
+
+audioPlayer.addEventListener("pause", () => {
+  const pauseButton = document.querySelector(
+    ".audioPlayer .audioPlayer-controls .audioPlayer-controls-playbackButton"
+  );
+
+  playButton.setAttribute("title", "Play")
+  pauseButton.setAttribute("src", "http://localhost:6060/icons/play.svg");
+});
+
+
